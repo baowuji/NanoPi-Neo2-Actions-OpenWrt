b44a236dad52ff462a5e057a50a26d3b68803768
baowuji <huangw@sibet.ac.cn>
2023-03-17 14:10:21 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -14,6 +14,9 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-echo 'src-git passwall https://github.com/WYC-2020/openwrt-passwall.git' >>feeds.conf.default
-echo 'src-git NanoHatOLED https://github.com/vinewx/NanoHatOLED.git' >>feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git passwall https://github.com/WYC-2020/openwrt-passwall.git' >>feeds.conf.default
+#echo 'src-git NanoHatOLED https://github.com/vinewx/NanoHatOLED.git' >>feeds.conf.default
+
+sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
